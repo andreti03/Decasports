ab52e3c752ff594054c9922dc3d1902cc3efb5da
CamiloGomezV <gomezvargas38@gmail.com>
2020-11-08 00:19:07 -0500

    actualizando tablas

--- delivery/models.py
+++ delivery/models.py
@@ -7,6 +7,6 @@ from tradesman.models import Tradesman
 class Delivery(models.Model):
     delivery_id = models.IntegerField(primary_key=True)
     delivery_date = models.DateField()
-    bill_number = models.ForeignKey(Bill, on_delete=mdoels.CASCADE)
+    bill_number = models.ForeignKey(Bill, on_delete=models.CASCADE)
     customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
     employer_id = models.ForeignKey(Tradesman, on_delete=models.CASCADE)
