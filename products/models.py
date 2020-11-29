7ed39df2d108a4288145c705452ae9d272164a95
andreti03 <andreti03@gmail.com>
2020-11-28 22:25:37 -0500

    Agregar image model product

--- products/models.py
+++ products/models.py
@@ -9,6 +9,7 @@ class Product(models.Model):
     price = models.IntegerField()
     product_sport_id = models.ForeignKey(Sport, on_delete=models.CASCADE)
     admin_id = models.ForeignKey(Admi, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
-        return self.product_name, self.price
+        return self.product_name
