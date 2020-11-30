b9b1f6dd714d61c598fed3713276c2a75f7861b7
Cuervinho <andresfe.cuervo@urosario.edu.co>
2020-11-30 01:20:26 -0500

    cambiar car_product por shopping_cart

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def Home(request):
     return render(request, 'shopping_cart/index.html')
+
+def index(request):
+    return render(request, 'shopping_cart/index.html')
