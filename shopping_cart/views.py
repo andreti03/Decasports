c3fc10bc920405f8e15f82e191f1d9dc247e26a3
Cuervinho <andresfe.cuervo@urosario.edu.co>
2020-11-30 01:29:52 -0500

    -

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -2,15 +2,12 @@ from django.shortcuts import render
 from categories.models import Sport
 
 def Home(request):
-<<<<<<< HEAD
     return render(request, 'shopping_cart/index.html')
 
 def index(request):
     return render(request, 'shopping_cart/index.html')
-=======
     sports = Sport.objects.all()
     context={
         'sport': sports
     }
     return render(request, 'shopping_cart/index.html', context)
->>>>>>> 5f9847c9a587d54331faa05e9bc77d282be37982
