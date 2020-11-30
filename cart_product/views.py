81ed2034710f3033b96a2d860af736df76927163
Cuervinho <andresfe.cuervo@urosario.edu.co>
2020-11-29 22:36:41 -0500

    Nada

--- cart_product/views.py
+++ cart_product/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from products.models import Product
+from.cart import Cart
 # Create your views here.
+
+def add_product(request,product_id)
+    cart = Cart(request)
+    product = Product.objects.get(id=product_id)
+    cart.add(product=product)
+    
